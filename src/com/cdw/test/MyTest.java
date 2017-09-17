fd5b75643e0d2f91116801824febd10e0493ff66
Github-Cdw <2670629111@qq.com>
2017-09-17 13:53:28 +0800

    Hello GitHub try two

--- src/com/cdw/test/MyTest.java
+++ src/com/cdw/test/MyTest.java
@@ -2,6 +2,6 @@ package com.cdw.test;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub Test");
     }
 }
